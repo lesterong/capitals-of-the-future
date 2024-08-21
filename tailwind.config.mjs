f14d5b438568009e16d05e7ccfaac3a35d6965e1
Lester Ong <lesterong776@gmail.com>
2024-08-22 02:48:48 +0800

    Update font weights

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,11 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontWeight: {
+			...defaultTheme.fontWeight,
+			normal: '325',
+			bold: '625',
+		},
 		extend: {
 			fontFamily: {
         sans: ['Plus Jakarta Sans Variable', ...defaultTheme.fontFamily.sans],
