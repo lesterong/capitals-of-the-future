25ae0e9fae63ec6ddd1a2032c2ec97168403a7b8
Lester Ong <lesterong776@gmail.com>
2024-08-22 23:59:32 +0800

    Redirect events

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/events/1': '/events'
+  },
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()]
 });
