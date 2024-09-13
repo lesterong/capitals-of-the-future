e5adc280b2625fe91a2c9c3b29826bb6b3b2a157
Lester Ong <lesterong776@gmail.com>
2024-09-13 12:10:04 +0800

    Update color scheme for individual posts

--- src/components/CardTags.tsx
+++ src/components/CardTags.tsx
@@ -3,23 +3,25 @@ import { SlashIcon } from "@heroicons/react/24/solid";
 const CardTags = ({
   type,
   tags = [],
+  light = false,
 }: {
   type: "events" | "news" | "research";
   tags?: string[];
+  light?: boolean;
 }) => {
   return (
     <div className="flex items-center space-x-1">
       <a
         href={`/${type}`}
-        className="text-sm font-bold uppercase transition-colors text-primary-700 hover:text-primary-900 duration-100"
+        className={`text-sm font-bold uppercase transition-colors duration-100 ${light ? 'text-primary-300 hover:text-primary-400' : 'text-primary-700 hover:text-primary-900'}`}
       >
         {type}
       </a>
-      <SlashIcon className="size-4 fill-gray-700/40" />
+      <SlashIcon className={`size-4 ${light ? 'fill-gray-300/60' : 'fill-gray-700/40'}`} />
       <div className="shrink flex flex-wrap gap-1">
         {tags.sort().map((tag) => (
           <a
-            className="text-sm bg-gray-100 border border-gray-200/70 px-2 py-0.5 rounded-lg transition-all hover:border-primary-700 active:scale-95"
+            className="text-sm bg-gray-100 border border-gray-200/70 px-2 py-0.5 rounded-lg transition-all text-gray-900 hover:border-primary-700 active:scale-95"
             href={`/${type}?city=${tag}`}
             key={tag}
           >
