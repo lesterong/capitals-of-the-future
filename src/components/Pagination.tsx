63b2843181a6a0b3cd66000266069669eac81cdf
Lester Ong <lesterong776@gmail.com>
2024-09-13 12:16:21 +0800

    Update pagination color scheme

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -23,14 +23,14 @@ const Pagination = ({
   return (
     <div className="text-gray-700">
       <button
-        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-sky-100"
+        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-primary-100"
         onClick={() => handlePageChange(1)}
         disabled={page === 1}
       >
         <ChevronDoubleLeftIcon className="size-4 stroke-current" />
       </button>
       <button
-        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-sky-100"
+        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-primary-100"
         disabled={page === 1}
         onClick={() => handlePageChange(page - 1)}
       >
@@ -39,14 +39,20 @@ const Pagination = ({
       {pagination.map((p) => {
         if (p === "...") {
           return (
-            <button key={p} disabled className="inline-flex items-center px-2 h-10 rounded-lg">
+            <button
+              key={p}
+              disabled
+              className="inline-flex items-center px-2 h-10 rounded-lg"
+            >
               {p}
             </button>
           );
         }
         return (
           <button
-            className={`inline-flex items-center px-2 h-10 hover:bg-sky-100 rounded-lg transition-colors ${page === p ? "bg-sky-800 text-white hover:bg-sky-800" : ""}`}
+            className={`inline-flex items-center px-2 h-10 hover:bg-primary-100 rounded-lg transition-colors
+              ${page === p ? "bg-primary-800 text-white hover:bg-primary-800" : ""}
+              `}
             onClick={() => handlePageChange(p)}
             key={p}
           >
@@ -55,14 +61,14 @@ const Pagination = ({
         );
       })}
       <button
-        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-sky-100"
+        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-primary-100"
         disabled={page === totalPages}
         onClick={() => handlePageChange(page + 1)}
       >
         <ChevronRightIcon className="size-4 stroke-current" />
       </button>
       <button
-        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-sky-100"
+        className="inline-flex items-center px-2 h-10 rounded-lg transition-colors disabled:text-gray-400 enabled:hover:bg-primary-100"
         disabled={page === totalPages}
         onClick={() => handlePageChange(totalPages)}
       >
