0fb3a84bcbba316adb7135a5f391ac7672cdd447
Lester Ong <lesterong776@gmail.com>
2024-09-16 00:00:03 +0800

    Clean up code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    "/events/1": "/events",
-  },
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
